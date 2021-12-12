7400a3fff955be7930bbea69bd5d81a92d59b961
Jeffrey Meng <jeffkmeng@gmail.com>
2021-12-11 16:18:52 -0800

    wip image downloading

---
+++ src/htmlTemplate.ts
@@ -0,0 +1,68 @@
+export default function wrapHTML(body: string) {
+
+    return `
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Jeffrey Meng">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jeffkmeng.com/library/bootstrap/4.1.3/css/bootstrap.min.css">
+    <title>Jeffrey Template</title>
+    <style>
+        /* https://stackoverflow.com/a/3525675/5511561 */
+
+        html,
+        body {
+            margin: 0px;
+            padding: 0px;
+            min-height: 100%;
+            height: 100%;
+        }
+
+        .container {
+            min-height: 100%;
+            height: auto !important;
+            margin-bottom: -50px;
+            /* the bottom margin is the negative value of the footer's total height */
+        }
+
+        .container:after {
+            content: "";
+            display: block;
+            height: 50px;
+            /* the footer's total height */
+        }
+
+
+
+        .footer {
+            height: 50px;
+            /* the footer's total height */
+        }
+
+        .footer-content {
+
+            height: 32px;
+            /* height + top/bottom padding + top/bottom border must add up to footer height */
+            padding: 8px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-lg-12 pt-3">
+            ${body}
+        </div>
+    </div>
+    <script src="https://cdn.jeffkmeng.com/library/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="https://cdn.jeffkmeng.com/library/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+    `
+}
--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,105 @@
-import axios from "axios";
 import { promises as fs } from "fs";
-import * as queryString from "query-string";
-import * as dotenv from "dotenv";
-import { channel } from "diagnostics_channel";
+import { config as dotenvConfig } from "dotenv";
+import dayjs from "dayjs";
+import escapeHtml from "escape-html";
+import axios from "axios";
+import {
+	ConversationsHistoryResponse,
+	ConversationsListResponse,
+	UsersListResponse,
+	WebClient,
+} from "@slack/web-api";
+import { Member } from "@slack/web-api/dist/response/UsersListResponse";
+import getHTMLFile from "./htmlTemplate";
+import wrapHTML from "./htmlTemplate";
 
-dotenv.config();
+dotenvConfig();
 
-let counter = 0;
-async function get(
-	path: string,
-	options: Record<string, any>
-): Promise<Record<string, any>> {
-	return await axios
-		.get(
-			`https://slack.com/api${path}?${queryString.stringify({
-				...options,
-			})}`,
-			{
-				headers: {
-					Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
-				},
-			}
-		)
-		.then((response) => response.data)
-		.catch((error) => {
-			if (error.response) {
-				console.log(error.response.data);
-				console.log(error.response.status);
-				console.log(error.response.headers);
-			} else if (error.request) {
-				console.log(error.request);
-			} else {
-				console.log("Error", error.message);
-			}
-			console.log(error.config);
-		});
-}
-async function paginatedGet(
-	path: string,
+// Initialize
+const slackapi = new WebClient(process.env.SLACK_TOKEN);
+
+async function paginate(
+	method: string,
 	key: string,
-	options: Record<string, any>,
-	startCursor?: string
+	options: Record<string, any> = {},
+	onPageGet?: (page: Record<string, any>[]) => void
 ): Promise<Record<string, any>[]> {
-	const data = await get(path, {
+	const paginator = slackapi.paginate(method, {
+		name: key,
+		limit: 250, // can be overridden in options
 		...options,
-		cursor: startCursor,
 	});
 
-	const cursor = data.response_metadata?.next_cursor;
-
-	counter++;
-	console.log(`got data: ${counter}`);
-
-	if (!data[key]) {
-		throw new Error("Expected data with key: " + key);
+	let data: Record<string, any>[] = [];
+	for await (const page of paginator) {
+		if (onPageGet) {
+			onPageGet(page[key] as Record<string, any>[]);
+		}
+		data = [...data, ...(page[key] as Record<string, any>[])];
 	}
+	return data;
+}
 
-	if (!cursor) {
-		return data[key];
-	} else {
-		return [
-			...data[key],
-			...(await paginatedGet(path, key, options, cursor)),
-		];
+async function getMembers() {
+	const members: UsersListResponse["members"] = await paginate(
+		"users.list",
+		"members"
+	);
+	if (!members) {
+		throw new Error("Unable to fetch members.");
 	}
+	return members;
+}
+async function getChannels() {
+	const channels: ConversationsListResponse["channels"] = await paginate(
+		"conversations.list",
+		"channels",
+		{
+			types: "public_channel,private_channel,mpim,im",
+		}
+	);
+	if (!channels) {
+		throw new Error("Unable to fetch channels");
+	}
+	return channels;
 }
 
-async function getMembers(): Promise<Record<string, any>[]> {
-	return paginatedGet("/users.list", "members", {
-		limit: 250,
-	});
-}
-
-async function getChannels(): Promise<Record<string, any>[]> {
-	return paginatedGet("/conversations.list", "channels", {
-		types: "public_channel,private_channel,mpim,im",
-	});
-}
-async function getChannel(id: string): Promise<Record<string, any>> {
-	return {
-		info: (
-			await get("/conversations.info", {
-				channel: id,
-			}).then((data) => {
-				if (!data.channel) {
-					console.log("NO CHANNEL");
-					console.log(data);
-				}
-				return data;
-			})
-		).channel,
-		messages: await paginatedGet("/conversations.history", "messages", {
-			channel: id,
+async function getChannelHistory(
+	channelId: string,
+	onPageGet?: (page: Record<string, any>[]) => void
+) {
+	const history: ConversationsHistoryResponse["messages"] = await paginate(
+		"conversations.history",
+		"messages",
+		{
+			channel: channelId,
 			limit: 1000, // per page
-		}),
+		},
+		onPageGet
+	);
+	if (!history) {
+		throw new Error("Unable to fetch history for channel ID " + channelId);
+	}
+	return history;
+}
+async function getChannel(
+	id: string,
+	onPageGet?: (page: Record<string, any>[]) => void
+) {
+	return {
+		info: await slackapi.conversations
+			.info({
+				channel: id,
+			})
+			.then((data) => {
+				if (!data.channel) {
+					throw new Error(
+						"Unable to fetch channel data for id " + id
+					);
+				}
+				return data.channel;
+			}),
+		messages: await getChannelHistory(id, onPageGet),
 	};
 }
 
@@ -122,12 +128,32 @@ function getType(channelObj: Record<string, any>): ChannelType {
 		throw new Error("Unexpected channel type");
 	}
 }
+
+let lastProgressMessageLength = -1;
+
+async function updateProgress(message: string) {
+	if (lastProgressMessageLength === -1) {
+		process.stdout.write(message);
+	} else {
+		process.stdout.write(
+			"\r" +
+				message +
+				Array(Math.max(0, lastProgressMessageLength - message.length))
+					.fill(" ")
+					.join("")
+		);
+	}
+
+	lastProgressMessageLength = message.length;
+}
 async function exportAllChannels(): Promise<void> {
+	updateProgress("Setting up output directories...");
 	await fs.rm("./out", {
 		force: true,
 		recursive: true,
 	});
 	await fs.mkdir("./out");
+
 	await Promise.all(
 		[
 			"./out/public-channels",
@@ -139,7 +165,7 @@ async function exportAllChannels(): Promise<void> {
 			await fs.mkdir(path + "/archived");
 		})
 	);
-	console.log("done creating stuff");
+	updateProgress("Fetching channels and members...");
 
 	const [channels, members] = await Promise.all([
 		getChannels().then((channels) => {
@@ -159,44 +185,170 @@ async function exportAllChannels(): Promise<void> {
 	]);
 
 	const memberIdMap = members.reduce(
-		(obj, member) => ({
+		(obj: Record<string, Member>, member: Member) => ({
 			...obj,
-			[member.id]: {
-				username: member.name,
-				realName: member.real_name,
-				email: member.profile.email,
-				image: member.profile.image_512,
-			},
+			[member.id as string]: member,
 		}),
 		{}
 	);
+	let numMessagesExported = 0;
+	let numChannelsExported = 0;
+	updateProgress("Exporting...");
+	await Promise.all(
+		channels.map(async (channel) => {
+			const id = channel.id;
 
-	for (const channel of channels) {
-		const id = channel.id;
-		const type = getType(channel);
-		if (type == ChannelType.INDIVIDUAL_DM && !memberIdMap[channel.user]) {
-			console.log(channel);
-		}
-		// console.log(memberIdMap)
-		const name =
-			type === ChannelType.INDIVIDUAL_DM
-				? `dm_${memberIdMap[channel.user].realName}`
-				: channel.name_normalized;
-		const archived = channel.is_archived;
+			if (!id) {
+				throw new Error("Unable to get ID of channel.");
+			}
 
-		console.log(`Fetching channel ${channelTypeMap[type]} ${name}__${id}`);
+			const type = getType(channel);
+			if (
+				type == ChannelType.INDIVIDUAL_DM &&
+				!memberIdMap[
+					(channel as typeof channel & { user: string })
+						.user as string
+				]
+			) {
+				// console.log(channel);
+			}
+			// console.log(memberIdMap)
+			const name =
+				type === ChannelType.INDIVIDUAL_DM
+					? `dm_${
+							memberIdMap[
+								(channel as typeof channel & { user: string })
+									.user as string
+							].real_name
+					  }`
+					: channel.name_normalized;
+			const archived = channel.is_archived;
 
-		const channelData = await getChannel(id);
-		fs.writeFile(
-			`./out/${channelTypeMap[type]}${
-				archived ? "/archived" : ""
-			}/${name}__${id}.json`,
-			JSON.stringify(channelData, null, 4)
-		);
-	}
+			// console.log(
+			// 	`Fetching channel ${channelTypeMap[type]} ${name}__${id}`
+			// );
+
+			const channelData = await getChannel(id, (messages) => {
+				numMessagesExported += messages.length;
+				updateProgress(
+					`Exported ${numMessagesExported} messages // ${numChannelsExported} channels`
+				);
+			});
+			numChannelsExported++;
+			updateProgress(
+				`Exported ${numMessagesExported} messages // ${numChannelsExported} channels`
+			);
+			fs.writeFile(
+				`./out/${channelTypeMap[type]}${
+					archived ? "/archived" : ""
+				}/${name}__${id}.json`,
+				JSON.stringify(channelData, null, 4)
+			);
+		})
+	);
+	// for (const channel of channels) {
+	//
+	// }
 }
+
+async function processChannelHistory(
+	history: Record<string, any>
+): Promise<string> {
+	const members = await getMembers();
+	const files: Record<string, any>[] = [];
+	const memberIdMap = members.reduce(
+		(obj: Record<string, Member>, member: Member) => ({
+			...obj,
+			[member.id as string]: member,
+		}),
+		{}
+	);
+	history.messages.forEach((message: Record<string, any>) => {
+		if (message.files) {
+			files.push(...message.files);
+		}
+	});
+	console.log("fetching files");
+	await Promise.all(
+		files.map((f) => {
+			if (!f.url_private_download) {
+				console.log(`File with no URL: ${f.id} // ${f.name}`);
+				return Promise.resolve();
+			}
+			return axios
+				.get(f.url_private_download, {
+					responseType: "arraybuffer",
+					headers: {
+						Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+					},
+				})
+				.then((response) =>
+					fs.writeFile(
+						"./out/private-channels/chat__G977U7MDJ__files/" +
+							`${f.id}__${f.name}`,
+						response.data
+					)
+				)
+				.catch((e) => {
+					console.log(e);
+					console.log(f);
+				});
+		})
+	);
+	console.log("processing messages");
+	const processedMessages = history.messages.map(
+		(message: Record<string, any>) =>
+			`<p>${dayjs
+				.unix(parseFloat(message.ts))
+				.format("M/D/YY h:mm A")} <b>${
+				escapeHtml(memberIdMap[message.user]?.real_name) ||
+				`Unknown user ${escapeHtml(message.user)}`
+			}:</b> ${
+				escapeHtml(
+					message.text.replace(/&gt;/g, ">").replace(/&lt;/g, "<")
+				).replace(/\n/g, "<br/>") || "<NO TEXT>"
+			} ${
+				message.files != undefined
+					? `(${message.files.length} attached file${
+							message.files.length !== 1 ? "s" : ""
+					  }) ${message.files
+							.filter((f) => f.mimetype.indexOf("image") > -1)
+							.map(
+								(f) => `
+						  <img src="${
+								"./chat__G977U7MDJ__files/" +
+								`${f.id}__${f.name}`
+							}"/>`
+							)
+							.join("")}`
+					: ""
+			}</p>`
+	);
+
+	return wrapHTML(`
+<h1>${history.info.name} </h1>
+<p>Created ${dayjs
+		.unix(parseFloat(history.info.created))
+		.format("M/D/YY h:mm A")}</p>
+<p>All times reported in UTC${dayjs().utcOffset() / 60}:${
+		dayjs().utcOffset() % 60 == 0 ? "00" : dayjs().utcOffset() % 60
+	}</p>
+<hr/>
+<div class="messages">
+${processedMessages.join("\n")}
+</div>
+`);
+}
+
 async function main() {
 	// console.log(await getMembers());
-	exportAllChannels();
+	// exportAllChannels();
+	fs.readFile("./out/private-channels/chat__G977U7MDJ.json")
+		.then((data) => JSON.parse(data + ""))
+		.then((obj) => processChannelHistory(obj))
+		.then((txt) =>
+			fs.writeFile("./out/private-channels/chat__G977U7MDJ.html", txt)
+		);
 }
+
 main();
